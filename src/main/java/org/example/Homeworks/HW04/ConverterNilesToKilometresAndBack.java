73611964ec55d938de8399e4c17877c63259028d
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-05 16:55:46 +0300

    hw04 ConverterNilesToKilometresAndBack

---
+++ src/main/java/org/example/Homeworks/HW04/ConverterNilesToKilometresAndBack.java
@@ -0,0 +1,24 @@
+package org.example.Homeworks.HW04;
+
+public class ConverterNilesToKilometresAndBack {
+    static double MilesToKilometres(double M)
+    {
+        return M * 1.609;
+    }
+    static double KilometrestoMiles(double K)
+    {
+        return K / 1.609;
+    }
+    // Driver function
+    public static void main (String[] args)
+    {
+        double M = 200;
+        double K = 50;
+
+        System .out.println ( "\n" +
+                "Distance in kilometers ( K ) = "
+                + MilesToKilometres(M));
+        System .out.println ( "Mileage ( M ) = "
+                + KilometrestoMiles(K));
+    }
+}
