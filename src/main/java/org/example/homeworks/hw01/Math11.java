cd7b7e3ff9e2a196ac7720b2d6d4fda8df8b20a4
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:00:27 +0300

    hw01 Math11

---
+++ src/main/java/org/example/homeworks/hw01/Math11.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+public class Math11 {
+    public static void main(String[] args) {
+
+        double x = 11.;
+        double y = 2*PI*x- abs(sin(sqrt(10.5*x)))*1/(pow(pow(x, 2+0.14),1. / 3));
+
+
+        System.out.println(y);
+    }
+}
