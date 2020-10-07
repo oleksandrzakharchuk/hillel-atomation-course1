3b155189ee21d197348068b62911b4a2e22e97c5
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 12:43:53 +0300

    hw04 ComverterpoundsToKilogramsAndBack

---
+++ src/main/java/org/example/homeworks/hw04/ComverterpoundsToKilogramsAndBack.java
@@ -0,0 +1,26 @@
+package org.example.homeworks.hw04;
+
+public class ComverterpoundsToKilogramsAndBack {
+    static double PoundsToKilomgrams(double P)
+    {
+        return P / 2.205;
+    }
+    static double KilomgramstoPoubds (double K)
+    {
+        return K * 2.205;
+    }
+    // Driver function
+    public static void main (String[] args)
+    {
+        double P = 2;
+        double K = 5;
+
+        System .out.println ( "\n" +
+                "weight in kilograms ( K ) = "
+                + PoundsToKilomgrams(P));
+        System .out.println ( "\n" +
+                "weight in pounds ( P ) = "
+                + KilomgramstoPoubds(K));
+    }
+}
+
