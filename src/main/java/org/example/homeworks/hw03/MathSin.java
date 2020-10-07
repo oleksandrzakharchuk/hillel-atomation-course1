707527538a994a8b3aa53e6f8b4bf8fe882cc734
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 12:08:57 +0300

    hw03 MathSin

---
+++ src/main/java/org/example/homeworks/hw03/MathSin.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw03;
+
+public class MathSin {
+
+    public static void main(String[] args)
+    {
+        for (int i=0; i<360; i+=10 )
+
+            System.out.println("sin " +i+ " гадусов = " + Math.sin(i));
+
+    }
+}
