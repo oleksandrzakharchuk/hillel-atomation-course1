aa041abf23300ec739cc85f45508ac09a95647e6
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 07:26:46 +0300

    hw01 ConvertCF

--- src/main/java/org/example/Homeworks/HW05/CountWordsOfEachLength.java
+++
@@ -1,8 +0,0 @@
-package org.example.Homeworks.HW05;
-
-public class CountWordsOfEachLength {
-    public static void main(String[] args) {
-        String randomString = "";
-        System.out.println("String has " + randomString.split("").length + " word");
-    }
-}
