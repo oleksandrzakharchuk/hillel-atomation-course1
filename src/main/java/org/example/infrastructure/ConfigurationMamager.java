83ea6825cd59c32407f615450950212feeebcd1b
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 19:32:59 +0300

    to infrastructure ConfigurationMamager

--- src/main/java/org/example/infrastructure/ConfigurationMamager.java
+++ src/main/java/org/example/infrastructure/ConfigurationMamager.java
@@ -1,4 +1,33 @@
 package org.example.infrastructure;
 
 public class ConfigurationMamager {
+    private static ConfigurationMamager instance;
+
+    private ConfigurationMamager() {
+
+    }
+
+    private static ConfigurationMamager getInstance() {
+        if (instance == null) {
+            instance = new ConfigurationMamager();
+        }
+        return instance;
+    }
+
+    public String getTestBrowser() {
+
+        return getEnvironmentVariableOrDefault("testBrowser", "chrome");
+
+    }
+
+    public String getTestEnv() {
+        return getEnvironmentVariableOrDefault("testEnv", "production");
+    }
+
+    String getEnvironmentVariableOrDefault(String envVar, String defoultVolue) {
+        return System.getenv(envVar) == null || System.getenv(envVar).isBlank() ?
+                defoultVolue :
+                System.getenv(envVar);
+    }
 }
+
