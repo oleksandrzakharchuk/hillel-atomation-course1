73969871d6ee226c7ab244e2d1950724ab7fff0f
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 06:34:10 +0300

    hw04 07 infrastruc

--- src/main/java/org/example/Homeworks/infrastructure/ConfigurationMamager.java
+++
@@ -1,9 +0,0 @@
-package org.example.Homeworks.infrastructure;
-
-public class ConfigurationMamager {
-    public static String getEnvironmentVariableOrDefault(String envVar, String defaultValue) {
-        return System.getenv(envVar)==null  ?
-                defaultValue :
-                System.getenv(envVar);
-    }
-}
