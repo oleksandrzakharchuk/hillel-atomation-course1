b16fc33d9af7f1e78a4dd7f45ce553a2ce5ee325
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 23:42:19 +0300

    hw09TestApp

--- src/main/java/org/example/homeworks/hw09/TestApp1.java
+++ src/main/java/org/example/homeworks/hw09/TestApp1.java
@@ -6,20 +6,20 @@ import org.example.infrastructure.WebDriverManager;
 
 public class TestApp1 {
     public static void main(String[] args) {
-        TestLogger logger = new TestLogger();
+        TestLogger logger=new TestLogger();
 
         logger.log("Launch browser");
-        WebDriverManager wdm = new WebDriverManager();
-        String browser = wdm.createBrowser();
+        WebDriverManager wdm=new WebDriverManager();
+        String brawser= wdm.createBrowser();
 
         logger.log("Open website");
-        System.out.println("-->" + browser + " opening " + TestServer.GetUrl());
+        System.out.println("-->"+brawser+"opening"+ TestServer.GetUrl());
 
         logger.log("Precondition1");
-        System.out.println("-->Step 1");
+        System.out.println("-->Step1");
 
         logger.log("Precondition2");
-        System.out.println("-->Step 2");
+        System.out.println("-->Step2");
 
         logger.log("Precondition3");
         System.out.println("-->Step3");
@@ -27,8 +27,8 @@ public class TestApp1 {
         logger.log("Check smth");
         System.out.println("-->Chek text on page");
 
-        logger.log("Close browser ");
-        wdm.closeBrowser(browser);
+        logger.log("Close browser");
+        wdm.closeBrowser(brawser);
     }
 }
 
