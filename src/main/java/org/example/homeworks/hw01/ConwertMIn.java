5be0b3c2f9d72eef0818e5e15f50350aee7a1eea
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 07:55:08 +0300

    hw01 ConvertMIn

---
+++ src/main/java/org/example/homeworks/hw01/ConwertMIn.java
@@ -0,0 +1,16 @@
+package org.example.homeworks.hw01;
+
+public class ConwertMIn {
+    public static void main(String[] args) {
+        double m=1000;
+        double in=1000;
+        double avg=1000*39.37;
+
+        System.out.println(avg);
+
+        System.out.println(1000/39.37);
+        //Перевод метров в дюймы (метры * 39,37)
+        //Перевод перевод дюймов в метры (дюймы / 39,37)
+
+    }
+}
