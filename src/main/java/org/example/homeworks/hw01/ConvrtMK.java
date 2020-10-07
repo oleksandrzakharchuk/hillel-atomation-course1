bf324a6122558b21916eec5eb5c77fdcedf441b4
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 07:52:47 +0300

    hw01 ConvertMK

---
+++ src/main/java/org/example/homeworks/hw01/ConvrtMK.java
@@ -0,0 +1,14 @@
+package org.example.homeworks.hw01;
+
+public class ConvrtMK {
+    public static void main(String[] args) {
+        double mi=1000;
+        double km=1000;
+        double avg= 1000*1.609;
+
+        System.out.println(avg);
+        System.out.println(1000/1.609);
+    }
+}
+//перевод мили в километры, (мили*1.609)
+// перевод километры в мили, (км/1.609)
