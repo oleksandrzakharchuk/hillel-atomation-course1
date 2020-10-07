58c8f4b10f0c5e55940879fd5f20552d8ca3ff60
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 15:15:11 +0300

    hw5 CountWordsOfEachLength

---
+++ src/main/java/org/example/homeworks/hw05/CountWordsOfEachLength.java
@@ -0,0 +1,8 @@
+package org.example.homeworks.hw05;
+
+public class CountWordsOfEachLength {
+    public static void main(String[] args) {
+        String randomString = "";
+        System.out.println("String has " + randomString.split("").length + " word");
+    }
+}
