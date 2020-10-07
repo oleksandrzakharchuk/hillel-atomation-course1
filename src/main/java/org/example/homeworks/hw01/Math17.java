fa255c1a20f41b2a99bb538717e6702450615a3f
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 10:09:12 +0300

    hw01 Math17

---
+++ src/main/java/org/example/homeworks/hw01/Math17.java
@@ -0,0 +1,10 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+public class Math17 {
+    public static void main(String[] args) {
+        double x = 17;
+        double y = pow(x,3)/3-pow(E,x)*log(abs(pow(1.3,3)+pow(x,3)))+(1.3);
+
+        System.out.println(y);
+    }
+}
