2967f848372aaff22e9cfe19bd430d18f3bb0b4d
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 10:05:57 +0300

    hw01 MultiplecHoice

---
+++ src/main/java/org/example/homeworks/hw01/MultiplecHoice.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw01;
+
+public class MultiplecHoice {
+    public static void main(String[] args) {
+        int numberwrong=10;
+        int numbercorrect=18;
+
+        double finale= (18-(0.25*10));
+
+        System.out.println(finale);
+    }
+}
