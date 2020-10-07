a694c57fe367028e8c3b925f4b72ce024f22aa89
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:17:47 +0300

    hw01 Math10

---
+++ src/main/java/org/example/homeworks/hw01/Math10.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+public class Math10 {
+    public static void main(String[] args) {
+
+        double x = 1.;
+        double y = 0.3*sqrt(abs(sin(x)))*pow(pow(E, 0.12 * x), 1. / 3);
+
+
+        System.out.println(y);
+    }
+}
