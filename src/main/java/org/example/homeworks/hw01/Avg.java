99c5275224d101c401f45bb594a8a5e6d9166859
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:26:06 +0300

    hw01 Avg

---
+++ src/main/java/org/example/homeworks/hw01/Avg.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw01;
+
+public class Avg {
+
+    public static void main(String[] args) {
+        int a=6;
+        int b=12;
+        int c=17;
+        double avg=(6+12+17)/3.;
+        System.out.println(avg);
+    }
+}
