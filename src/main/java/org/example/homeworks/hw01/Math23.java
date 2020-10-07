bfcb9159855e9096e65e0c5cab4806163b88c38e
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:07:12 +0300

    hw01 Math23

---
+++ src/main/java/org/example/homeworks/hw01/Math23.java
@@ -0,0 +1,11 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+
+public class Math23 {
+    public static void main(String[] args) {
+        double x=23;
+
+        double y = (acos(tan(5*x/PI)))-((pow(x,5.7))/23);
+        System.out.println(y);
+    }
+}
