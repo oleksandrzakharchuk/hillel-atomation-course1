a58309607a45f1ec162f065063df9a9ffd4266c3
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 11:56:10 +0300

    hw02 KvadratnoeU

---
+++ src/main/java/org/example/homeworks/hw02/KvadratnoeU.java
@@ -0,0 +1,39 @@
+package org.example.homeworks.hw02;
+
+public class KvadratnoeU {
+    public static void main(String[] args) {
+        double a = 1;
+        double b = -13;
+        double c = 36;
+
+        double d = (b * b) - (4 * a * c);
+        System.out.println("Решение квадратного уравнения");
+
+        System.out.print("Введите значение a -> ");
+
+        System.out.print("Введите значение b -> ");
+
+        System.out.print("Введите значение c -> ");
+
+        System.out.println("a = " + a + " b = " + b + " c = " + c);
+
+
+        System.out.println("Дискриминант = " + d);
+
+        if (d < 0)
+            System.out.print("Уравнение не имеет решений!!!");
+        else if (d == 0) {
+            double x1 = (-b) / (2 * a);
+            System.out.println("x = " + x1);
+        } else {
+            double x1 = (-b + Math.sqrt(d)) / (2 * a);
+            double x2 = (-b - Math.sqrt(d)) / (2 * a);
+            if (x1 == x2)
+                System.out.println("x1 = x2 " + x1);
+            else
+                System.out.println("x1 =  " + x1 + "  x2 = " + x2);
+
+        }
+    }
+}
+//a x2+b x+c = 0
