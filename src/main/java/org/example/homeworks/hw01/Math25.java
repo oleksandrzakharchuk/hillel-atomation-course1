ffc7fcd1224ab6a3ec22ce64ccf2a58a5263ffd8
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:11:38 +0300

    hw01 Math25

---
+++ src/main/java/org/example/homeworks/hw01/Math25.java
@@ -0,0 +1,11 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+public class Math25 {
+    public static void main(String[] args) {
+
+        double x=25;
+        double y= (pow(log10(acos(abs(pow(x,3.4)+(2.5)*pow(x,1.2)-(0.7)))/(pow((pow(E,2.5*x)),1./3))),1./4)+1);
+        System.out.println(y);
+    }
+}
+
