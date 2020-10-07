d78a481b8235c2336c89449c11b3dfe11db48801
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:28:13 +0300

    hw01 Math14

---
+++ src/main/java/org/example/homeworks/hw01/Math14.java
@@ -0,0 +1,13 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+public class Math14 {
+    public static void main(String[] args) {
+
+        double x = 14;
+        double y = ((sqrt(pow(sin(x/2),3)))+(pow(E, 1.3 * x))+pow(E,-1.3 * x)) * (1/(abs( x+2.5)));
+
+
+        System.out.println(y);
+    }
+
+}
