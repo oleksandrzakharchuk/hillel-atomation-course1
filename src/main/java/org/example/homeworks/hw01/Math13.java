a9463ac0920f14d53588beeba47998caeef3fd8f
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 08:04:57 +0300

    hw01 Math13

---
+++ src/main/java/org/example/homeworks/hw01/Math13.java
@@ -0,0 +1,12 @@
+package org.example.homeworks.hw01;
+import static java.lang.Math.*;
+public class Math13 {
+    public static void main(String[] args) {
+
+        double x = 13.;
+        double y = pow((pow(E, -2 + x)), 1. / 5) * (1/pow(x, 2)+pow(x,4))+log(abs(x-3.14));
+
+
+        System.out.println(y);
+    }
+}
