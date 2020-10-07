9b78ef02ebed0becc6f5fe4370d1e3278f536c3b
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 10:13:09 +0300

    hw01 Math16

---
+++ src/main/java/org/example/homeworks/hw01/Math16.java
@@ -0,0 +1,17 @@
+package org.example.homeworks.hw01;
+
+import static java.lang.Math.*;
+
+public class Math16 {
+
+    public static void main(String[] args) {
+
+        double x = 1.;
+        double y = pow((pow(E, 2 * x) * sqrt(x) - (x + 1. / 3) / x), 1. / 3) * (abs(cos(2.5 * x)));
+
+
+        System.out.println(y);
+
+    }
+}
+
