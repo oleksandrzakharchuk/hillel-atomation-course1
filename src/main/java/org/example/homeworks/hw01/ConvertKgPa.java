5860841dee62d593b8aa324453430c47fee10675
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 07:47:48 +0300

    hw01 ConvertKgPa

---
+++ src/main/java/org/example/homeworks/hw01/ConvertKgPa.java
@@ -0,0 +1,15 @@
+package org.example.homeworks.hw01;
+
+public class ConvertKgPa {
+    public static void main(String[] args) {
+        double f=10;
+        double k=1;
+
+        double avg=1*2.205;
+
+        System.out.println(avg);
+        System.out.println(10/2.205);
+    }
+}
+//Перевод килограмов в футны (кг*2.205),
+// перевод фунтов в килограмы (фунты/ 2,205)
