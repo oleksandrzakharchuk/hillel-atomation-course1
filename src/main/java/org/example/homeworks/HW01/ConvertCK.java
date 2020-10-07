ced6117d17a03f8e8978fe26e7b4ffdd09f528f7
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-07 06:44:19 +0300

    hw01 ConvertCF

---
+++ src/main/java/org/example/homeworks/HW01/ConvertCK.java
@@ -0,0 +1,10 @@
+package org.example.homeworks.HW01;
+
+public class ConvertCK {
+    public static void main(String[] args) {
+        double c=0.5;
+        double avg=0.5+273.15;
+        System.out.println(avg);
+    }
+}
+//0,5 °C + 273,15 = 273,15 K
