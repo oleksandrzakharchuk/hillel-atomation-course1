a5d7b4d8f704c3a2117cd67c8da36498c4b6ff4e
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-12 15:33:21 +0300

    infrastructure

---
+++ src/main/java/org/example/infrastructure/TestUrl.java
@@ -0,0 +1,35 @@
+package org.example.infrastructure;
+
+public class TestUrl {
+    public static class Builder {
+
+        public String withProtocol(String protocol) {
+
+        }
+
+        public String withDomain(String domain) {
+        }
+
+        public String withPort(String port) {
+        }
+
+        public String withPath(String path) {
+
+        }
+
+        public String withParm(String parm) {
+
+        }
+
+        public String withParm(String key, String value) {
+
+        }
+
+        public String build(return url;) {
+
+        }
+    }
+
+}
+
+
