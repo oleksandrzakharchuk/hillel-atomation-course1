51aabe11d90b44373756daa2c468e608abf86922
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-12 18:35:10 +0300

    hw12

---
+++ src/main/java/org/example/homeworks/hw12/Builder.java
@@ -0,0 +1,28 @@
+package org.example.homeworks.hw12;
+
+public class Builder {
+    void setType(Type type) {
+
+    }
+
+
+ void setSeats(int seats) {
+    }
+
+
+
+    void setTransmission(Transmission transmission) {
+
+    }
+
+    void  setTripComputer
+(TripComputer tripComputer) {
+    }
+
+    void setGPSNavigator(GPSNavigator gpsNavigator) {
+
+    }
+
+    public void setEngine(Engine engine) {
+    }
+}
---
+++ src/main/java/org/example/homeworks/hw12/Type.java
@@ -0,0 +1,5 @@
+package org.example.homeworks.hw12;
+
+public enum Type {
+    CITY_CAR, SPORTS_CAR, SUV
+}
