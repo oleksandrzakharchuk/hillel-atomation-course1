d9bd6eaff8d751a1db31ed2b8812d4310f88a655
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-12 15:55:02 +0300

    infrastructure

--- src/main/java/org/example/infrastructure/TestUrl.java
+++ src/main/java/org/example/infrastructure/TestUrl.java
@@ -4,29 +4,33 @@ public class TestUrl {
     public static class Builder {
 
         public String withProtocol(String protocol) {
-
+return "protocol";
         }
 
         public String withDomain(String domain) {
+            return "domain";
         }
 
         public String withPort(String port) {
+            return "port";
         }
 
         public String withPath(String path) {
+            return "path";
 
         }
 
         public String withParm(String parm) {
-
+return "parm";
         }
 
         public String withParm(String key, String value) {
+return "key";
 
         }
 
         public String build(return url;) {
-
+return "url";
         }
     }
 
