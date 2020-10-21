5138a9b2b081bbe56419afe67b024c23796fedc5
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-10-21 14:45:41 +0300

    Multiply

---
+++ src/main/java/org/example/codewars/Multiply.java
@@ -0,0 +1,10 @@
+package org.example.codewars;
+//This code does not execute properly. Try to figure out why.
+//
+//BUGS
+public class Multiply {
+    public static Double multiply(Double a, Double b) {
+        return a * b;//;- bug
+    }
+}
+
