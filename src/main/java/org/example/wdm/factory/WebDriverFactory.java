7dc9d687c956919f2c597b75617db8543a9e85ed
Oleksandr Zakharchuk <zahirolek@gmail.com>
2020-11-30 11:10:28 +0200

    hw factory and capabilites ex

---
+++ src/main/java/org/example/wdm/factory/WebDriverFactory.java
@@ -0,0 +1,31 @@
+package org.example.wdm.factory;
+
+/*
+import com.wixpress.automation.webdriver.config.WebDriverConfig;
+import com.wixpress.automation.webdriver.enums.WebDriverType;
+import org.openqa.selenium.WebDriver;
+
+public class WebDriverFactory {
+    */
+/**
+     * Creates a web driver according to given browser type
+     *
+     * @param webDriverType web driver type
+     * @return web driver
+     *//*
+
+    WebDriver create(WebDriverType webDriverType);
+
+    */
+/**
+     * Creates a web driver according to browser type from configuration build parameters
+     *
+     * @return web driver
+     *//*
+
+    default WebDriver create() {
+        WebDriverType webDriverType = WebDriverType.fromString(WebDriverConfig.getInstance().getWebDriverType());
+        return create(webDriverType);
+    }
+}
+*/
